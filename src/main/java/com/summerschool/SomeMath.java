48c3d98d15b23001ca8df900446ecfac2410713c
Alexey Korkota <alexey.korkota@gmail.com>
2014-06-01 14:19:21 +0400

    Done.

--- src/main/java/com/summerschool/SomeMath.java
+++ src/main/java/com/summerschool/SomeMath.java
@@ -4,14 +4,22 @@ public class SomeMath {
     public static double awesomeFunction(double a, double b, double c, double d) {
         double result = (a + b) / (c - d);
 
-        if(Double.isInfinite(result)) {
-            return (result > 0) ? Double.MAX_VALUE : Double.MIN_VALUE;
+        // Path #1.
+        if (result == Double.POSITIVE_INFINITY) {
+            return Double.MAX_VALUE;
         }
 
+        // Path #2.
+        if (result == Double.NEGATIVE_INFINITY) {
+            return -Double.MAX_VALUE;
+        }
+
+        // Path #3.
         if(Double.isNaN(result)) {
             return 0;
         }
 
+        // Path #4.
         return result;
     }
 }
--- src/test/java/com/summerschool/TestSomeMath.java
+++ src/test/java/com/summerschool/TestSomeMath.java
@@ -6,21 +6,11 @@ import org.junit.Test;
 public class TestSomeMath {
     private static final double delta = 0.0;
 
+    /**
+     * Testing of the path #1.
+     */
     @Test
-    public void testAwesomeFunctionWhenResultShouldBeFiniteNumber() {
-        double a = 21;
-        double b = 21;
-        double c = 4;
-        double d = 3;
-
-        double result = SomeMath.awesomeFunction(a, b, c, d);
-        double expected = 42;
-
-        assertEquals(expected, result, delta);
-    }
-
-    @Test
-    public void testAwesomeFunctionWhenResultShouldBePositiveInfinite() {
+    public void testAwesomeFunctionWhenResultShouldBeMaxValue() {
         double a = 1;
         double b = 2;
         double c, d;
@@ -32,26 +22,48 @@ public class TestSomeMath {
         assertEquals(expected, result, delta);
     }
 
+    /**
+     * Testing of the path #2.
+     */
     @Test
-    public void testAwesomeFunctionWhenResultShouldBeNegativeInfinite() {
+    public void testAwesomeFunctionWhenResultShouldBeMinValue() {
         double a = -1;
         double b = -2;
         double c, d;
         c = d = 4;
 
         double result = SomeMath.awesomeFunction(a, b, c, d);
-        double expected = Double.MIN_VALUE;
+        double expected = -Double.MAX_VALUE;
 
         assertEquals(expected, result, delta);
     }
 
+    /**
+     * Testing of the path #3.
+     */
     @Test
-    public void testAwesomeFunctionWhenResultShouldBeNaN() {
+    public void testAwesomeFunctionWhenResultShouldBeZero() {
         double a, b, c, d;
         a = b = c = d = 0;
 
         double result = SomeMath.awesomeFunction(a, b, c, d);
         double expected = 0;
+
+        assertEquals(expected, result, delta);
+    }
+
+    /**
+     * Testing of the path #4.
+     */
+    @Test
+    public void testAwesomeFunctionWhenResultShouldBeFiniteNumber() {
+        double a = 21;
+        double b = 21;
+        double c = 4;
+        double d = 3;
+
+        double result = SomeMath.awesomeFunction(a, b, c, d);
+        double expected = 42;
 
         assertEquals(expected, result, delta);
     }
